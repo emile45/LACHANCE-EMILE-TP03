a493c0ed5fc9ad982c49e39a5dcebd8711163130
Emile Lachance <1926353@cegepat.qc.ca>
2021-05-14 13:19:26 -0400

    Améliorations
    Amélioration de la map et ajout d'une tourelle murale qui peut tuer le joueur.
    Et autres choses.

--- Assets/Scripts/wallShooter.cs
+++ Assets/Scripts/wallShooter.cs
@@ -5,25 +5,56 @@ using UnityEngine;
 public class wallShooter : MonoBehaviour
 {
     public Transform barrel;
+    public Transform playerRb;
+    private Animation animationUpDown;
+    public GameObject bullet;
+    private float timeSince=0f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animationUpDown = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        TryDetectLayer();
 
-        
     }
-    void Shoot()
+    void TryDetectLayer()
     {
-        // Créer un rayon qui pointe vers l'avant du pistolet
-        Ray pistolRay = new Ray(barrel.position, barrel.forward);
-        RaycastHit hit;
+        if ((Time.time - timeSince) > 8f || timeSince == 0f)
+        {
+            animationUpDown.enabled = true;
+            // Créer un rayon 
+            Ray ray = new Ray(barrel.position, barrel.up * 50f);
+            RaycastHit hit;
 
+            // S'il est obstrué par un collider autre que le personnage
+            if (Physics.Raycast(ray, out hit, 50f))
+            {
+                // Verification pour savoir s'il s'agit du joueur.
+                if (hit.collider.CompareTag("Player"))
+                {
+                    animationUpDown.enabled = false;
+                    Shoot();
+                    timeSince = Time.time;
+                }
+            }
+        }
+
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet_ = Instantiate(bullet, new Vector3(barrel.position.x, barrel.position.y, barrel.position.z+2f), Quaternion.identity);
+        Rigidbody rbBullet = bullet_.GetComponent<Rigidbody>();
+        rbBullet.isKinematic = false;
+        rbBullet.AddForce(new Vector3(0f, 0f, 5f), ForceMode.Impulse);
+        Destroy(bullet_, 8f);
 
     }
     void OnDrawGizmos()
@@ -31,5 +62,7 @@ public class wallShooter : MonoBehaviour
         Gizmos.color = Color.magenta;
         Gizmos.DrawRay(barrel.position, barrel.up * 50f);
     }
+
+    
 
 }
